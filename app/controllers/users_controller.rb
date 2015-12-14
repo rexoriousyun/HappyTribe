c26d0c002da31849bdcd3ec6a68cc8fe356cd7f9
margaretyjkim <margaret.yj.kim@gmail.com>
2015-12-14 17:13:56 -0500

    created edit page for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   skip_before_action :require_login, only: [:new, :create]
+  before_action :get_user, only: [:edit, :show, :update]
 
   def new
     @user = User.new
@@ -30,6 +31,9 @@ class UsersController < ApplicationController
   end
 
   private
+  def get_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
