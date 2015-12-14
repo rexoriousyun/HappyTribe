324bc25de8f5c365a75f480c63e6ec992bcd9357
margaretyjkim <margaret.yj.kim@gmail.com>
2015-12-14 16:58:59 -0500

    added signup and login pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ActiveRecord::Base
   authenticates_with_sorcery!
 	has_many :arrangements
 	has_many :opportunities, through: :arrangements
+
+	validates :password, length: { minimum: 3 }
+	validates :password, confirmation: true
+	validates :password_confirmation, presence: true
+
+	validates :email, uniqueness: true
+
 end
