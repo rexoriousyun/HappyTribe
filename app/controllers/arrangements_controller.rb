bdcc3c515bbcd9d8d078ca33cd5faaa7c079ca39
ZainW <zainthegreatcb@gmail.com>
2015-12-14 18:58:02 -0500

    broken arr-form

--- app/controllers/arrangements_controller.rb
+++ app/controllers/arrangements_controller.rb
@@ -1,8 +1,19 @@
 class ArrangementsController < ApplicationController
+
+
   def new
   end
 
   def create
+    get_opportunity
+    @arrangement = @opportunity.arrangements.build(arr_params)
+    @arrangement.user = current_user
+    if @arrangement.save
+      puts "SUCESS!"
+    else
+      puts "failure"
+    end
+
   end
 
   def show
@@ -15,5 +26,12 @@ class ArrangementsController < ApplicationController
   end
 
   def destroy
+  end
+  private
+  def get_opportunity
+    @opportunity = Opportunity.find(params[:id])
+  end
+  def arr_params
+    params.require(:arrangement).permit(:time_slot, :opportunity_id)
   end
 end
