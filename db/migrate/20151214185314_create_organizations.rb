12e5057013b9eaac561b24ee47c1a171d292053a
margaretyjkim <margaret.yj.kim@gmail.com>
2015-12-14 13:54:21 -0500

    Added Organization model and ran migration

---
+++ db/migrate/20151214185314_create_organizations.rb
@@ -0,0 +1,12 @@
+class CreateOrganizations < ActiveRecord::Migration
+  def change
+    create_table :organizations do |t|
+      t.string :name
+      t.string :email
+      t.string :website_url
+      t.text :description
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151214184946) do
+ActiveRecord::Schema.define(version: 20151214185314) do
+
+  create_table "organizations", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "website_url"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
