d0e2c19e0a60ad9df026705adb8ebe8e1c38328f
James McGeachie <jamesmcgeachie10@gmail.com>
2015-12-17 14:07:57 -0500

    Added functionality to remove an interest from a User's interests collection.

--- app/controllers/interests_users_controller.rb
+++ app/controllers/interests_users_controller.rb
@@ -1,9 +1,14 @@
 class InterestsUsersController < ApplicationController
 
 	def update
-		@interest = Interest.find(params[:interest]
+		@interest = Interest.find(params[:interest])
+		if current_user.interests.include?(Interest.find(@interest))
+			current_user.interests.delete(@interest)
+			redirect_to user_path(current_user)
+		else
 			current_user.interests << @interest
-			redirect_to events_path
+			redirect_to user_path(current_user)
+		end
 	end
 
 end
