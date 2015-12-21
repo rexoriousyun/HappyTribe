14b2f68fa67f3bcec06a25b11789e93a859413ac
rex yun <chgchg123@naver.com>
2015-12-21 14:15:27 -0500

    ordered in user show 'skills and interest'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :require_login
+  before_filter :set_search
 
   def not_authenticated
   	redirect_to login_path, alert: "Please Login :)"
   end
+
+  def set_search
+		@search=Event.ransack(params[:q])
+	end
 end
