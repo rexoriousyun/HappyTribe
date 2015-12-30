d31171878846c0dc497ded7105356e9a9cdf80da
James McGeachie <jamesmcgeachie10@gmail.com>
2015-12-29 23:30:10 -0500

    Added new association between users and organizations

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,4 +1,4 @@
 class Organization < ActiveRecord::Base
 	has_many :events
-
+	belongs_to :event_coordinator, class_name: "User", foreign_key: "user_id"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,12 @@ class User < ActiveRecord::Base
   authenticates_with_sorcery!
 	has_many :arrangements
 	has_many :timeslots, through: :arrangements
-	# has_many :arranged_events, through: :timeslots, source: :event, class_name: "Event"
 	has_many :bookmarks
 	has_many :bookmarked_events, through: :bookmarks, source: :event, class_name: "Event"
+	has_many :managed_organizations, class_name: "Organization"
 	has_and_belongs_to_many :interests
 	has_and_belongs_to_many :skills
+
 
 
 	validates :password, length: { minimum: 3 }, if: :password_exists?
