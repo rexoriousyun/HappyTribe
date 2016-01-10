769d38e5b70dcd104c88733393d8b01f008ffb2a
rex yun <chgchg123@naver.com>
2016-01-10 17:09:32 -0500

    testing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => "smtp.sendgrid.net",
-    :port => "587",
+    :port => 587,
     :authentication => :plain,
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
