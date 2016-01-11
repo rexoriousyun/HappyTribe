e02a46350c8cb379a25a36470e9edac1dbb9f5a0
James McGeachie <jamesmcgeachie10@gmail.com>
2016-01-10 20:33:43 -0500

    Fixed syntax errors in seed file(s)

--- db/seeds/01_organization.rb
+++ db/seeds/01_organization.rb
@@ -24,41 +24,41 @@ toronto_humane_4 =  Organization.create(
   name: "Toronto Humane Society",
   email: "volunteer@torontohumanesociety.com",
   website_url: "http://www.torontohumanesociety.com",
-  description: "Features information about animal services, available animals for adoption, and fostering opportunities."
+  description: "Features information about animal services, available animals for adoption, and fostering opportunities.",
   user_id: 12
   );
 sick_kids_5 =  Organization.create(
   name: "Sick Kids",
   email: "sk.hr@sickkids.ca",
   website_url: "http://www.sickkids.ca/",
-  description: "The Hospital for Sick Children, also known as SickKids, is a major paediatric hospital located on University Avenue in Toronto, Ontario, Canada."
+  description: "The Hospital for Sick Children, also known as SickKids, is a major paediatric hospital located on University Avenue in Toronto, Ontario, Canada.",
   user_id: 13
   );
 canada_running_6 =  Organization.create(
   name: "Canada Running Series",
   email: "volunteer@canadarunningseries.com",
   website_url: "www.canadarunningseries.com",
-  description: "The Canada Running Series, an annual road racing series showcasing the world's premiere distance runners and amateur athletics in Canada."
+  description: "The Canada Running Series, an annual road racing series showcasing the world's premiere distance runners and amateur athletics in Canada.",
   user_id: 14
   );
 breast_cancer_7 =  Organization.create(
   name: "Canadian Breast Cancer Foundation",
   email:  "breastcancer@cbcf.org",
   website_url: "http://www.cbcf.org/",
-  description: "Learn more about breast cancer, get involved in upcoming events, volunteer or read more about our work towards creating a future without breast cancer."
+  description: "Learn more about breast cancer, get involved in upcoming events, volunteer or read more about our work towards creating a future without breast cancer.",
   user_id: 15
   );
 heart_and_stroke_8 =  Organization.create(
   name: "Heart and Stroke foundation",
   email:  "heartandstroke@hs.ca",
   website_url: "http://www.heartandstroke.on.ca/",
-  description: "The Heart and Stroke Foundation is one of Canada’s largest and most effective health charities. Over the last 60 years we have invested more than $1.39 billion in heart and stroke research."
+  description: "The Heart and Stroke Foundation is one of Canada’s largest and most effective health charities. Over the last 60 years we have invested more than $1.39 billion in heart and stroke research.",
   user_id: 16
   );
 toronto_environment_9 = Organization.create(
   name: "Toronto Environmental Alliance",
   email: "Torontoenv@talliance.ca",
   website_url: "http://www.torontoenvironment.org/",
-  description: "TEA focuses on actions to help Toronto prepare for the impacts of climate change by reducing energy use and investing in the right infrastructure."
+  description: "TEA focuses on actions to help Toronto prepare for the impacts of climate change by reducing energy use and investing in the right infrastructure.",
   user_id: 17
   );
