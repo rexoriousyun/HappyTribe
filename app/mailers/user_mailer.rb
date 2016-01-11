ad04ff7bf7905ed87fb621a7cb3b98315b4ec8a2
rex yun <chgchg123@naver.com>
2016-01-11 13:34:38 -0500

    emailing

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,12 +2,12 @@ class UserMailer < ApplicationMailer
 	default from: 'happytribeteam@gmail.com'
 
 	def welcome_email(user)
-		@user = user
-		mail(to: @user.email, subject: 'Welcome to HappyTribe')
+		#@user = user
+		mail(to: user.email, subject: 'Welcome to HappyTribe')
 	end
 
 	def registered_email(user)
 		@user = user
-		mail(to: @user.email, subject: 'Welcome to HappyTribe')
+		mail(to: @user.email, subject: "You have joined #{@user.arrangements.last.event.name} in HappyTribe")
 	end
 end
