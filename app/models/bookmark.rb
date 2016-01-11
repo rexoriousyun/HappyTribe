74095389730e59d8404dfb0489ebc8cd5cb87762
James McGeachie <jamesmcgeachie10@gmail.com>
2016-01-11 05:04:20 -0500

    Added check if bookmark exists to view, disabling button if it does

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,11 +1,15 @@
 class Bookmark < ActiveRecord::Base
   belongs_to :user
   belongs_to :event
-  validate :bookmarked_already?
+  validate :bookmark_exists_validation
 
-  def bookmarked_already?
-		if Bookmark.where(user_id: self.user_id).where(event_id: self.event_id).present?
-  		errors.add(:user_id, "has already bookmarked this event")
+  def bookmark_exists_validation
+    if self.bookmarked_already?(self.user, self.event)
+      errors.add(:user_id, "has already bookmarked this event")
   	end
+  end
+
+  def bookmarked_already?(user, event)
+    Bookmark.where(user_id: user.id).where(event_id: event.id).present?
   end
 end
