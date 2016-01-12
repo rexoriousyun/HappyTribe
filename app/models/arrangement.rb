adc6a590521adaf38a4ba289bb906ba134353a01
James McGeachie <jamesmcgeachie10@gmail.com>
2016-01-11 21:02:50 -0500

    Wrote validation for arrangement - timeslot capacity has been reached

--- app/models/arrangement.rb
+++ app/models/arrangement.rb
@@ -3,11 +3,18 @@ class Arrangement < ActiveRecord::Base
   belongs_to :timeslot
   has_one :event, through: :timeslot
   validate :already_booked?
+  validate :max_capacity?
 
   def already_booked?
   	if Arrangement.where(user_id: self.user_id).where(timeslot_id: self.timeslot_id).present?
   		errors.add(:user_id, "has already booked for this timeslot.")
   	end
+  end
+
+  def max_capacity?
+    if self.timeslot.remaining_spots == 0
+      errors.add(:timeslot_id, "Capacity has been reached for the timeslot you are trying to book")
+    end
   end
 
 end
