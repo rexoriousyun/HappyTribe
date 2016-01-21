62b4506f1a8efd581df486f175a6f613f4a88f99
margaretyjkim <margaret.yj.kim@gmail.com>
2016-01-20 23:39:10 -0500

    changed the dates of the seed file

--- db/seeds/03_timeslot.rb
+++ db/seeds/03_timeslot.rb
@@ -1,42 +1,42 @@
 timeslot_1 = Timeslot.create(
-  start_time: Time.new(2016, 1, 15, 9, 0, 0),
-  end_time: Time.new(2016, 1, 15, 12, 0, 0),
+  start_time: Time.new(2016, 5, 30, 9, 0, 0),
+  end_time: Time.new(2016, 5, 30, 12, 0, 0),
   capacity: 5,
   event_id: 1
   );
 timeslot_2 = Timeslot.create(
-  start_time: Time.new(2016, 1, 15, 12, 30, 0),
-  end_time: Time.new(2016, 1, 15, 15, 0, 0),
+  start_time: Time.new(2016, 7, 29, 12, 30, 0),
+  end_time: Time.new(2016, 7, 29, 15, 0, 0),
   capacity: 10,
   event_id: 1
   );
 timeslot_3 = Timeslot.create(
-  start_time: Time.new(2016, 1, 15, 15, 0, 0),
-  end_time: Time.new(2016, 1, 15, 18, 0, 0),
+  start_time: Time.new(2016, 8, 29, 15, 0, 0),
+  end_time: Time.new(2016, 8, 29, 18, 0, 0),
   capacity: 4,
   event_id: 2
   );
 timeslot_4 = Timeslot.create(
-  start_time: Time.new(2016, 1, 18, 9, 0, 0),
-  end_time: Time.new(2016, 1, 18, 12, 0, 0),
+  start_time: Time.new(2016, 9, 30, 9, 0, 0),
+  end_time: Time.new(2016, 9, 30, 12, 0, 0),
   capacity: 2,
   event_id: 2
   );
 timeslot_5 = Timeslot.create(
-  start_time: Time.new(2016, 1, 18, 12, 30, 0),
-  end_time: Time.new(2016, 1, 18, 15, 0, 0),
+  start_time: Time.new(2016, 10, 30, 12, 30, 0),
+  end_time: Time.new(2016, 10, 30, 15, 0, 0),
   capacity: 3,
   event_id: 3
   );
 timeslot_6 = Timeslot.create(
-  start_time: Time.new(2016, 1, 18, 15, 0, 0),
-  end_time: Time.new(2016, 1, 18, 18, 0, 0),
+  start_time: Time.new(2016, 11, 30, 15, 0, 0),
+  end_time: Time.new(2016, 11, 30, 18, 0, 0),
   capacity: 5,
   event_id: 3
   );
 timeslot_7 = Timeslot.create(
-  start_time: Time.new(2016, 1, 20, 9, 0, 0),
-  end_time: Time.new(2016, 1, 20, 12, 0, 0),
+  start_time: Time.new(2016, 12, 30, 9, 0, 0),
+  end_time: Time.new(2016, 12, 30, 12, 0, 0),
   capacity: 2,
   event_id: 4
   );
@@ -53,20 +53,20 @@ timeslot_9 = Timeslot.create(
   event_id: 5
   );
 timeslot_10 = Timeslot.create(
-  start_time: Time.new(2016, 1, 25, 15, 0, 0),
-  end_time: Time.new(2016, 1, 25, 18, 0, 0),
+  start_time: Time.new(2016, 3, 25, 15, 0, 0),
+  end_time: Time.new(2016, 3, 25, 18, 0, 0),
   capacity: 5,
   event_id: 6
   );
 timeslot_11 = Timeslot.create(
-  start_time: Time.new(2016, 1, 25, 17, 0, 0),
-  end_time: Time.new(2016, 1, 25, 20, 0, 0),
+  start_time: Time.new(2016, 3, 25, 17, 0, 0),
+  end_time: Time.new(2016, 3, 25, 20, 0, 0),
   capacity: 1,
   event_id: 6
   );
 timeslot_12 = Timeslot.create(
-  start_time: Time.new(2016, 1, 22, 8, 0, 0) ,
-  end_time: Time.new(2016, 1, 22, 11, 0, 0),
+  start_time: Time.new(2016, 3, 22, 8, 0, 0) ,
+  end_time: Time.new(2016, 3, 22, 11, 0, 0),
   capacity: 5,
   event_id: 7
   );
